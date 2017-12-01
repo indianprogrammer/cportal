fe7e9d467850023ba960e866e30241c471b23dfc
root <indianprogrammer.in@gmail.com>
2017-12-01 22:11:30 +0530

    capative portal login icon local

--- application/modules/capative_portal/controllers/Capative_portal.php
+++ application/modules/capative_portal/controllers/Capative_portal.php
@@ -4,16 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Capative_portal extends MX_Controller {
 
+    private $uamsecret = '123456';
+
     public function __Construct() {
         parent::__Construct();
         //$this->load->model('Token_model');
     }
 
     public function index() {
+        #collect get requestes
+        $res = $this->input - get('res');
+        $uamip = $this->input - get('uamip');
+        $uamport = $this->input - get('uamport');
+        $challenge = $this->input - get('challenge');
+        $called = $this->input - get('called');
+        $mac = $this->input - get('mac');
+        $ip = $this->input - get('ip');
+        $nasid = $this->input - get('nasid');
+        $userurl = $this->input - get('userurl');
+        $md = $this->input - get('md');
+        
+        
+        
+
+
         $loginData = array(
-            'title'=>'Login ISPjet'
+            'title' => 'Login ISPjet',
+            'uamip' => $uamip,
+            'uamport' => $uamport
         );
-        $this->load->view('login',$loginData);
+        $this->load->view('login', $loginData);
     }
 
 }

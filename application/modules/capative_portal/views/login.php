7a2c646f068c4fa6a34dd451af1648209e7c2d53
root <indianprogrammer.in@gmail.com>
2017-12-03 16:38:53 +0530

    pappassword test

--- application/modules/capative_portal/views/login.php
+++ application/modules/capative_portal/views/login.php
@@ -405,7 +405,7 @@
                 </div>
 
                 <!-- Login Form -->
-                <form action="capative_portal/login_action">
+                <form action="capative_portal/login_action" method="post">
                     <input type="hidden" name="challenge" value="<?= $challenge ?>">
                     <input type="hidden" name="uamip" value="<?= $uamip ?>">
                     <input type="hidden" name="uamport" value="<?= $uamport ?>">
